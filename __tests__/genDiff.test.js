088a18a64b5fd33fb0d7bb673181890b2f83be17
Александа Синицына <alexsandra217@mail.ru>
2026-09-01 01:34:42 +0500

    update workflows and update __tests__

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -5,7 +5,6 @@ import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
 const getPathUse = filename =>
   path.join(__dirname, '..', '__fixtures__', filename)
 
@@ -24,5 +23,15 @@ test('test perfect', () => {
 })
 
 test('test with different format', () => {
-  expect(() => genDiff(getPathUse('file1.json'), getPathUse('file.txt'))).toThrow()
+  expect(() =>
+    genDiff(getPathUse('file1.json'), getPathUse('file.txt')),
+  ).toThrow()
+})
+
+test('test with empty json', () => {
+  const result = `{
+}`
+  expect(
+    genDiff(getPathUse('fileNull1.json'), getPathUse('fileNull2.json')),
+  ).toBe(result)
 })
