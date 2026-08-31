c5236eba09a70e29eb58b4abc551fa6ed350c564
Александа Синицына <alexsandra217@mail.ru>
2026-09-01 00:47:03 +0500

    add  __tests__

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,28 @@
+import { expect, test } from 'vitest'
+import genDiff from '../src/index.js'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const getPathUse = filename =>
+  path.join(__dirname, '..', '__fixtures__', filename)
+
+test('test perfect', () => {
+  const result = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`
+  expect(genDiff(getPathUse('file1.json'), getPathUse('file2.json'))).toBe(
+    result,
+  )
+})
+
+test('test with different format', () => {
+  expect(() => genDiff(getPathUse('file1.json'), getPathUse('file.txt'))).toThrow()
+})
