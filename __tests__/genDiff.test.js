722f9a33af48410860e1f65d9c467a0e86ce9efc
Александа Синицына <alexsandra217@mail.ru>
2026-09-02 15:44:04 +0500

    return state Coverage

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -8,6 +8,20 @@ const __dirname = path.dirname(__filename)
 const getPathUse = (filename) =>
   path.join(__dirname, '..', '__fixtures__', filename)
 
+test('test perfect', () => {
+  const result = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`
+  expect(genDiff(getPathUse('file1.json'), getPathUse('file2.json'))).toBe(
+    result,
+  )
+})
+
 test('test with different format', () => {
   expect(() =>
     genDiff(getPathUse('file1.json'), getPathUse('file.txt')),
