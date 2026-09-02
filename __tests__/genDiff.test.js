e1283417496a61114abc668a331484fb4637fb30
Александа Синицына <alexsandra217@mail.ru>
2026-09-02 15:38:17 +0500

    update vitest.config.js for check Coverage

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -5,22 +5,8 @@ import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const getPathUse = filename =>
+const getPathUse = (filename) =>
   path.join(__dirname, '..', '__fixtures__', filename)
-
-test('test perfect', () => {
-  const result = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`
-  expect(genDiff(getPathUse('file1.json'), getPathUse('file2.json'))).toBe(
-    result,
-  )
-})
 
 test('test with different format', () => {
   expect(() =>
--- vitest.config.js
+++ vitest.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  coverage: {
-    thresholds: {
-      lines: 80,
-      functions: 80,
-      branches: 80,
-      statements: 80,
+  test: {
+    coverage: {
+      provider: 'v8',
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
     },
   },
 })
