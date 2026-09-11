5d61e3579eda25d7feec14ac699124f1df1d2ba9
Александа Синицына <alexsandra217@mail.ru>
2026-09-11 21:43:20 +0500

    update fixtures for tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -6,54 +6,13 @@ import genDiff from '../src/index.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
 const getPathUse = filename =>
   path.join(__dirname, '..', '__fixtures__', filename)
 
 test('test perfect functional stylish', () => {
-  const result = `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`
+  const result = fs.readFileSync(getPathUse('expected-stylish'), 'utf-8')
+
   expect(genDiff(getPathUse('file1.json'), getPathUse('file2.json'))).toBe(
     result,
   )
@@ -61,17 +20,7 @@ test('test perfect functional stylish', () => {
 })
 
 test('test perfect functional plain', () => {
-  const result = `Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]`
+  const result = fs.readFileSync(getPathUse('expected-plain'), 'utf-8')
 
   expect(
     genDiff(getPathUse('file1.json'), getPathUse('file2.json'), 'plain'),
@@ -82,7 +31,7 @@ Property 'group3' was added with value: [complex value]`
 })
 
 test('test perfect functional json', () => {
-  const result = fs.readFileSync(getPathUse('expected.json'), 'utf-8')
+  const result = fs.readFileSync(getPathUse('expected-json'), 'utf-8')
 
   expect(
     genDiff(getPathUse('file1.json'), getPathUse('file2.json'), 'json'),
