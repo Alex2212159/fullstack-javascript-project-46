ddc0cea880a9e479183542162fa46aa0ae02e262
Александа Синицына <alexsandra217@mail.ru>
2026-09-15 21:30:16 +0500

    update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,48 +1,40 @@
+import _ from 'lodash'
+
 const getNormalizeValue = (value) => {
   if (typeof value === 'string') {
     return `'${value}'`
   }
-  else if (
-    Object.prototype.toString.call(value) === '[object Object]'
-    || Array.isArray(value)
-  ) {
+  else if (_.isPlainObject(value) || Array.isArray(value)) {
     return '[complex value]'
   }
   return value
+}
+const typesDispatcher = {
+  nested: (elem, pathFull, func) => func(elem.children, pathFull),
+  changed: (elem, pathFull) => [
+    `Property '${pathFull}' was updated. From ${getNormalizeValue(elem.oldValue)} to ${getNormalizeValue(elem.newValue)}`,
+  ],
+  deleted: (elem, pathFull) => [`Property '${pathFull}' was removed`],
+  added: (elem, pathFull) => {
+    const value = elem.value !== undefined ? elem.value : elem.children
+    return [
+      `Property '${pathFull}' was added with value: ${getNormalizeValue(value)}`,
+    ]
+  },
 }
 
 const getPlainFormat = (tree = []) => {
   if (tree.length === 0) {
     return ''
   }
-
   const iters = (nodes, pathParents) => {
     const stringArray = nodes.flatMap((elem) => {
       const separator = '.'
       const { key, type } = elem
       const pathFull
         = pathParents === '' ? key : `${pathParents}${separator}${key}`
-
-      if (type === 'nested') {
-        return iters(elem.children, pathFull)
-      }
-      if (type === 'changed') {
-        return [
-          `Property '${pathFull}' was updated. From ${getNormalizeValue(elem.oldValue)} to ${getNormalizeValue(elem.newValue)}`,
-        ]
-      }
-      else if (type === 'deleted') {
-        return [`Property '${pathFull}' was removed`]
-      }
-      else if (type === 'added') {
-        const value = elem.value !== undefined ? elem.value : elem.children
-        return [
-          `Property '${pathFull}' was added with value: ${getNormalizeValue(value)}`,
-        ]
-      }
-      else {
-        return []
-      }
+      const action = typesDispatcher[type] || (() => [])
+      return action(elem, pathFull, iters)
     })
     return stringArray
   }
